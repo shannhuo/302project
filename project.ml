43b22014c1d30dabd4c393b48b56a7c81403efc8
Nicholas Hayek <hayekn1@outlook.com>
2024-11-16 15:34:26 -0500

    - made 2D functions consistent in format, structure, output. - 2D memo is broke - made some general functions tr - better formatting for general function outputs - signposting

--- project.ml
+++ project.ml
@@ -17,6 +17,11 @@ let string_of_bool_list_list lst_lst =
   "[" ^ String.concat "; " (List.map string_of_bool_list lst_lst) ^ "]"
 let string_of_2_bool_list_list lst_lst =
   "[" ^ String.concat "; " (List.map string_of_2_bool_list lst_lst) ^ "]"
+
+let make_value_pairs vars comb =
+  List.combine vars comb
+
+let combinations = [[true;true];[true;false];[false;true];[false;false]]
 let rec printExpression (e: expression): string = match e with
   |Var("a") -> "a"
   |Var("b") -> "b"
@@ -24,64 +29,85 @@ let rec printExpression (e: expression): string = match e with
   |And(u, v) -> "(" ^ printExpression u ^ " ∧ " ^ printExpression v ^ ")"
   |Or(u, v) -> "(" ^ printExpression u ^ " ∨ " ^ printExpression v ^ ")"
   |Var(_) -> raise invalidInput
-let rec trPrintExpression (e: expression) (acc: string -> string): string = match e with
-  |Var("a") -> acc "a"
-  |Var("b") -> acc "b"
-  |Not(u) -> trPrintExpression u (fun r -> acc ("¬" ^ r))
-  |And(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∧ " ^ r2 ^ ")")))
-  |Or(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∨ " ^ r2 ^ ")")))
-  |Var(_) -> raise invalidInput
-let printExpression' (e : expression) : string = trPrintExpression e (fun r -> r)
+
+let printExpression' (e: expression) : string =
+  let rec trPrintExpression (e: expression) (acc: string -> string): string = match e with
+    |Var("a") -> acc "a"
+    |Var("b") -> acc "b"
+    |Not(u) -> trPrintExpression u (fun r -> acc ("¬" ^ r))
+    |And(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∧ " ^ r2 ^ ")")))
+    |Or(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∨ " ^ r2 ^ ")")))
+    |Var(_) -> raise invalidInput in
+
+  trPrintExpression e (fun r -> r)
 
 
 (*EVALUATING*)
-let rec evaluateExpression (e: expression) (i : bool * bool) : bool = let (a, b) = i in
-  match e with 
-  |Var("a") -> a
-  |Var("b") -> b
-  |Not(u) -> not (evaluateExpression u i)
-  |And(u, v) -> let u' = (evaluateExpression u i) and v' = evaluateExpression v i in (u' && v')
-  |Or(u, v) -> let u' = (evaluateExpression u i) and v' = evaluateExpression v i in (u' || v')
-  |Var(_) -> raise invalidInput
-let rec trEvaluateExpression (e: expression) (i : bool * bool) (acc: bool -> bool): bool = let (a, b) = i in
-  match e with 
-  |Var("a") -> acc a
-  |Var("b") -> acc b
-  |Not(u) -> trEvaluateExpression u i (fun r -> acc (not r))
-  |And(u, v) -> trEvaluateExpression u i (fun r1 -> trEvaluateExpression v i (fun r2 -> acc (r1 && r2)))
-  |Or(u, v) -> trEvaluateExpression u i (fun r1 -> trEvaluateExpression v i (fun r2 -> acc (r1 || r2)))
-  |Var(_) -> raise invalidInput
-let evaluateExpression' (e: expression) (i : bool * bool) : bool = trEvaluateExpression e i (fun r -> r)
-let rec memoEvaluateExpression (e : expression) (i : bool * bool) =
+let evaluateExpression (e: expression) =
+  let pullValues e values = match values with 
+  | a::[b] -> (
+  let rec eval e = match e with 
+    |Var("a") -> a
+    |Var("b") -> b
+    |Not(u) -> not (eval u)
+    |And(u, v) -> let u' = (eval u) and v' = (eval v) in (u' && v')
+    |Or(u, v) -> let u' = (eval u) and v' = (eval v) in (u' || v')
+    |Var(_) -> raise invalidInput in
+    eval e)
+  |_-> raise invalidInput in
+  pullValues
+let evaluateExpression' (e : expression) =
+  let pullValues e values = match values with 
+  | a::[b] -> (
+    let rec trEval e (acc: bool -> bool) = match e with 
+      |Var("a") -> acc a
+      |Var("b") -> acc b
+      |Not(u) -> trEval u (fun r -> acc (not r))
+      |And(u, v) -> trEval u (fun r1 -> trEval v (fun r2 -> acc (r1 && r2))) 
+      |Or(u, v) -> trEval  u (fun r1 -> trEval v (fun r2 -> acc (r1 || r2)))
+      |Var(_) -> raise invalidInput in
+      trEval e (fun r -> r))
+  |_ -> raise invalidInput in
+  pullValues
+let rec memoEvaluateExpression (e : expression) =
   let store : (expression, bool) Hashtbl.t = Hashtbl.create 1000 in
-  let (a, b) = i in
-  let rec memoEval f =
-    match Hashtbl.find_opt store e with
-    | Some result -> result  
-    | None ->
-        let result = 
-          match f with
-          | Var("a") -> a
-          | Var("b") -> b
-          | Not u -> not (memoEval u)
-          | And (u, v) -> (memoEval u) && (memoEval v)
-          | Or (u, v) -> (memoEval u) || (memoEval v)
-          | Var(_) -> raise invalidInput
-      in (Hashtbl.add store f result; result)
-    in
-    let output = memoEval e in
-    (Hashtbl.iter (fun x y -> Printf.printf "%s -> %s\n" (printExpression x) (string_of_bool y)) store; output)
+  let pullValues e values = match values with
+  |a::[b] -> (
+    let rec memoEval f values = 
+      match Hashtbl.find_opt store e with
+      | Some result -> result  
+      | None ->
+          let result = 
+            match f with
+            | Var("a") -> a
+            | Var("b") -> b
+            | Not u -> not (memoEval u values)
+            | And (u, v) -> (memoEval u values) && (memoEval v values)
+            | Or (u, v) -> (memoEval u values) || (memoEval v values)
+            | Var(_) -> raise invalidInput
+        in (Hashtbl.add store f result; result)
+      in
+      memoEval )
+    |_ -> raise invalidInput in 
+    pullValues
 
+
+(* TRUTH TABLE GENERATION *)
+let truthTable2D evaluator (e : expression)  = 
+    let eval = evaluator e in
+    List.map (fun comb -> (comb, eval e comb)) combinations
+let printTruthTable2D evaluator (e : expression) =
+  let table = truthTable2D evaluator e in
+  Printf.printf "Truth table for %s:\n" (printExpression e);
+  Printf.printf "%12s | Result\n" "a   b";
+  List.iter (fun (comb, result) ->
+      Printf.printf "%12s | %B\n" (String.concat " " (List.map string_of_bool comb)) result
+    ) table 
+
+  ;;
 
 (*SAT SECTION*)
-let truthTable2D (e : expression) (evaluator : expression -> (bool * bool) -> bool): ((bool list) list) = 
-  let a00 = (true, true) in 
-  let a01 = (false, true) in
-  let a10 = (true, false) in
-  let a11 = (false, false) in
-    [[evaluator e a00; evaluator e a01];
-    [evaluator e a10; evaluator e a11]]
-let satSolverImplies (e1 : expression) (e2 : expression) evaluator : bool = 
+(*let satSolverImplies (e1 : expression) (e2 : expression) evaluator : bool = 
   let e = Or(Not(e1), e2) in
   let result = truthTable2D e evaluator in
   result = [[true;true];[true;true]]
@@ -107,7 +133,7 @@ let findSolutions (e: expression) evaluator : (bool * bool) list =
   |h::t -> if evaluator e h then findComb t (h::acc) else findComb t acc 
 in findComb comb []
 
- 
+ *)
 (*    TESTINGGGGGG   *)
 let test1 = And(Not(Or(Var("a"), And(Var("a"), Var("b")))), Or(And(Var("a"), Var("b")), And(Not(Var("a")), Var("b"))));;
 let test2 = And(Not(Var("a")), Var("b")) ;;
@@ -115,8 +141,11 @@ let test2 = And(Not(Var("a")), Var("b")) ;;
 let sat1 = And(Var("a"), Var("b"));;
 let sat2 = Var("a")
 let sat3 = Or(Var("a"), Var("b"))
-let sat4 = Not(And(Not(Var("a")), Not(Var("b"))))
-
+let sat4 = Not(And(Not(Var("a")), Not(Var("b"))));;
+printTruthTable2D evaluateExpression test2;;
+printTruthTable2D evaluateExpression' test2;;
+printTruthTable2D memoEvaluateExpression test2;;
+(*
 let test e = 
   let a = evaluateExpression e (true, true) in 
   let a' = evaluateExpression' e (true, true) in 
@@ -153,3 +182,5 @@ evaluateExpression' test2 (true, true);;
 printExpression test2;;
 printExpression' test2;;
 truthTable2D test2;;*)
+
+*)
--- projectGeneralized.ml
+++ projectGeneralized.ml
@@ -7,43 +7,58 @@ type expression =
   | And of expression * expression
   | Or of expression * expression
 
+
 (* PRINTING *)
 let string_of_bool_list lst =
   "[" ^ String.concat "; " (List.map string_of_bool lst) ^ "]"
-
 let string_of_bool_list_list lst_lst =
   "[" ^ String.concat "; " (List.map string_of_bool_list lst_lst) ^ "]"
-
-let rec printExpression (e: expression): string = match e with
-  | Var u -> string_of_int u
-  | Not u -> "¬" ^ printExpression u
+let rec printExpression (e: expression): string = match e with (* recursive *)
+  | Var(u) -> string_of_int u
+  | Not(u) -> "¬" ^ printExpression u
   | And(u, v) -> "(" ^ printExpression u ^ " ∧ " ^ printExpression v ^ ")"
   | Or(u, v) -> "(" ^ printExpression u ^ " ∨ " ^ printExpression v ^ ")"
+let printExpression' (e: expression) : string = (* with continuations *)
+  let rec trPrintExpression (e: expression) (acc: string -> string): string = match e with
+    |Var(u) -> acc (string_of_int u)
+    |Not(u) -> trPrintExpression u (fun r -> acc ("¬" ^ r))
+    |And(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∧ " ^ r2 ^ ")")))
+    |Or(u, v) -> trPrintExpression u (fun r1  -> trPrintExpression v (fun r2 -> acc ("(" ^ r1 ^ " ∨ " ^ r2 ^ ")"))) in
+  trPrintExpression e (fun r -> r)
+
 
 (* INPUT COLLECTION *)
-let inputList (e: expression): int list = 
+let inputList (e: expression): int list = (* recursive *)
   let rec matchInputs e acc = match e with
     | Var u -> if List.mem u acc then acc else u :: acc
     | Not u -> matchInputs u acc
     | And(u, v) | Or(u, v) -> matchInputs u (matchInputs v acc)
   in
-  matchInputs e []
+  List.sort (fun a b -> if a<b then 0 else 1) (matchInputs e [])
+let trInputList (e: expression): int list = (* with continuations *)
+  let rec matchInputs e acc sc = match e with
+    | Var u -> if List.mem u acc then sc acc else sc (u :: acc)
+    | Not u -> matchInputs u acc sc
+    | And(u, v) | Or(u, v) -> matchInputs u acc (fun r -> sc (matchInputs v r sc))
+  in
+  List.sort (fun a b -> if a<b then 0 else 1) (matchInputs e [] (fun r -> r)) (* sort by increasing order *)
+
+
 
 (* COMBINATION GENERATION *)
-let rec generate_combinations n =
+let rec generate_combinations n : (bool list) list = (* will generate in binary decreasing order *)
   if n = 0 then [[]]
   else
     let sub_combinations = generate_combinations (n - 1) in
-    List.concat [
-      List.map (fun comb -> true :: comb) sub_combinations;
+      List.map (fun comb -> true :: comb) sub_combinations @
       List.map (fun comb -> false :: comb) sub_combinations
-    ]
 
 let make_value_pairs vars comb =
   List.combine vars comb
 
+
 (* EVALUATING WITH MEMOIZATION *)
-let memoEvaluateExpression (e: expression) =
+let memoEvaluateExpression (e: expression) = (* partial evaluation *)
   let store : (expression * (int * bool) list, bool) Hashtbl.t = Hashtbl.create 1000 in
 
   let rec eval e values =
@@ -59,10 +74,11 @@ let memoEvaluateExpression (e: expression) =
           | Or(u, v) -> (eval u values) || (eval v values)
         in
         (* Memoize and return result *)
-        Hashtbl.add store (e, values) result;
-        result
+        (Hashtbl.add store (e, values) result;
+        result)
   in
   eval
+
 
 (* TRUTH TABLE GENERATION *)
 let truthTable (e : expression) : (bool list * bool) list =
@@ -74,19 +90,23 @@ let truthTable (e : expression) : (bool list * bool) list =
       (comb, eval e values)
     ) combinations
 
+(* format: list of ([var1=bool1, ..., varn=booln], eval)  *)   
+
 (* PRINTING THE TRUTH TABLE *)
 let print_truth_table (e : expression) =
   let vars = inputList e in
   let table = truthTable e in
   Printf.printf "Truth table for %s:\n" (printExpression e);
-  Printf.printf "%s | Result\n" (String.concat " " (List.map string_of_int vars));
+  Printf.printf "%12s | Result\n" (String.concat "    " (List.map string_of_int vars));
   List.iter (fun (comb, result) ->
-      Printf.printf "%s | %b\n" (String.concat " " (List.map string_of_bool comb)) result
+      Printf.printf "%12s | %B\n" (String.concat " " (List.map string_of_bool comb)) result
     ) table
 
 (* TESTING *)
 let test1 = And(Not(Or(Var(1), And(Var(1), Var(2)))), Or(And(Var(3), Var(2)), And(Not(Var(1)), Var(3))))
 let test2 = And(Not(Var(3)), Var(2))
+let test3 = And(And(Or(Or(Not(Var(3)), Var(10)), Or(Not(Var(3)), And(Var(2), Var(9)))), 
+Or(Var(1), Or(Or(Var(6), Var(5)), Var(2)))), Or(Or(Var(5), Or(Or(Var(8), Var(3)), Not(Var(5)))), Not(Var(10))))
 
 let () =
   printExpression test1 |> Printf.printf "Expression 1: %s\n";
